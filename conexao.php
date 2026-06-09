d2203a3da4940bffe1aff2aae3686b1d14040cbf
matheusdebritosilva <matheusbs20112009@gmail.com>
2026-06-09 15:32:37 -0300

    ultimas correções

--- conexao.php
+++ conexao.php
@@ -1,6 +1,5 @@
 
 <?php
-/*
 $servername= "localhost";
 $username= "root";
 $password= "";
@@ -12,20 +11,4 @@ if(!$conn){
     die("error" . mysqli_connect_error());
 } else {
 }
-
-$KM= $_POST['velocidade'] ?? '';
-$RPM= $_POST['rpm'] ?? '';
-
-
-
-$sql= "INSERT into dados (KM, RPM) values ('$KM', '$RPM')";
-
-if($conn->query($sql) === TRUE){
-    echo "New Record created successfuly";
-} else {
-    echo "Error" . $sql . "<br>" . $conn->error;
-}
-
-$conn->close();
-*/
 ?>
